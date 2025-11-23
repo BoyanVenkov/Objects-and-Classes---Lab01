f73a7dc95769cb60a034e1adcc2994777254fc4d
Boyan Venkov <boyanvenkov@yahoo.com>
2025-11-23 16:29:07 +0200

    Add project files.

---
+++ 01. Randomize Words/Program.cs
@@ -0,0 +1,10 @@
+﻿List<string> inputList = Console.ReadLine().Split().ToList();
+
+Random rnd = new Random();
+
+while (inputList.Count > 0)
+{
+    int num = rnd.Next(0, inputList.Count);
+    Console.WriteLine(inputList[num]);
+    inputList.RemoveAt(num);
+}
